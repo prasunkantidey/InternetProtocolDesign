414a4a8e2030ab93c3b3722c7792e437faa69e7e
prasunkantidey <prasun@nevada.unr.edu>
2016-02-26 19:57:09 -0800

    ITC config, read from file
    Defined ITC struct.
    Read from file into ITC

--- CPE701/ERTP/src/ERTP.cpp
+++ CPE701/ERTP/src/ERTP.cpp
@@ -1,15 +1,57 @@
 //============================================================================
 // Name        : ERTP.cpp
-// Author      : 
-// Version     :
+// Author      : Prasun
+// Version     : 1.0
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+struct ITCConfiguration{
+	int nodeId;
+	string nodeHostName;
+	int udpPort;
+	int firstConnectedNode;
+	int secondConnectedNode;
+	int linkMTU;
+};
+
+int main(int argc, char** argv) {
+	if(argc != 2){
+		std::cout << "Improper format! Try again!" << std::endl;
+		return 0;
+	}
+
+	ifstream fin(argv[1]);
+	vector<ITCConfiguration> itcConfigVector;
+
+	if(fin.good()){
+		while(!fin.eof()){
+			ITCConfiguration temp;
+			fin >> temp.nodeId >> temp.nodeHostName >> temp.udpPort
+				>> temp.firstConnectedNode >> temp.secondConnectedNode
+				>> temp.linkMTU;
+			itcConfigVector.push_back(temp);
+		}
+	}
+	else{
+		std::cout << "File can't file! Check proper file" << endl;
+	}
+	fin.close();
+
+////	Just for printing!!
+//	vector<ITCConfiguration>::iterator v = itcConfigVector.begin();
+//	while(v != itcConfigVector.end()){
+//		ITCConfiguration temp = *v;
+//		std::cout << temp.nodeId << ", " << temp.nodeHostName << std::endl;
+//		v++;
+//	}
+
 	return 0;
 }
